cc960eb80c23f15d9ec0730b3127a24fb273b82e
zakki <zakkimuzakki050@gmail.com>
2023-08-31 18:16:48 +0700

    4th commit

--- script/script.js
+++ script/script.js
@@ -5,12 +5,13 @@ function generateButtonNavbar(dataNavbar) {
         const buttonData = dataNavbar[i];
         const link = document.createElement("a");
         link.classList.add("button-navbar");
-        link.href = buttonData.id;
+        link.href = buttonData.id; // Use buttonData.id for the href (scroll to the element with this ID)
         link.textContent = buttonData.label;
 
         link.addEventListener("click", function(event) {
             event.preventDefault();
 
+            // Scroll to the element associated with the link
             const elementToScrollTo = document.querySelector(buttonData.id);
             if (elementToScrollTo) {
                 elementToScrollTo.scrollIntoView({ behavior: "smooth" });
@@ -22,8 +23,8 @@ function generateButtonNavbar(dataNavbar) {
 }
 
 function displayFormSubmission() {
-    const form = document.getElementById("myForm")
-    const pesanDiv = document.getElementById("pesan")
+    const form = document.getElementById("myForm"); // Gunakan ID formulir untuk memilihnya
+    const pesanDiv = document.getElementById("pesan");
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
